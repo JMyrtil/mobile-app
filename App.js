b8c09410def657dcf2fae964968e52560c91de49
Joel Myrtil <myrtil.joel@gmail.com>
2023-05-30 22:11:11 -0700

    lab 41

--- App.js
+++ App.js
@@ -1,11 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 
 export default function App() {
+  const [contact, setContact] = useState(null);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const getRandomContact = () => {
+    const contacts = [
+      { name: 'John Doe', phone: '1234567890' },
+      { name: 'Jane Smith', phone: '0987654321' },
+    ];
+
+    const randomIndex = Math.floor(Math.random() * contacts.length);
+    const selectedContact = contacts[randomIndex];
+
+    setContact(selectedContact);
+  };
+
+  const pickRandomPicture = () => {
+    const pictures = [
+      { uri: 'https://example.com/image1.jpg' },
+      { uri: 'https://example.com/image2.jpg' },
+    ];
+
+    const randomIndex = Math.floor(Math.random() * pictures.length);
+    const selectedPicture = pictures[randomIndex];
+
+    setSelectedImage(selectedPicture);
+  };
+
+  const sendPictureToContact = () => {
+    if (contact && selectedImage) {
+      console.log(`Sending picture ${selectedImage.uri} to contact ${contact.name}`);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {contact ? (
+        <View style={styles.contact}>
+          <Text style={styles.contactName}>{contact.name}</Text>
+          <Text style={styles.contactPhone}>{contact.phone}</Text>
+        </View>
+      ) : (
+        <Text style={styles.noContact}>No contact selected</Text>
+      )}
+
+      <Button title="Select random contact" onPress={getRandomContact} />
+      <Button title="Select random picture" onPress={pickRandomPicture} />
+      <Button title="Send picture" onPress={sendPictureToContact} disabled={!contact || !selectedImage} />
+
+      {selectedImage && <Image source={{ uri: selectedImage.uri }} style={styles.image} />}
     </View>
   );
 }
@@ -13,8 +58,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  contact: {
+    marginBottom: 20,
+  },
+  contactName: {
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  contactPhone: {
+    fontSize: 16,
+  },
+  noContact: {
+    fontSize: 18,
+    marginBottom: 20,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain',
+    marginTop: 20,
   },
 });
---
+++ README.md
@@ -0,0 +1,7 @@
+## Screenshot
+
+![Pic 1](lab41.png)
+
+![Pic 2](lab41(1).png)
+
+![Pic 3](lab41(2).png)
